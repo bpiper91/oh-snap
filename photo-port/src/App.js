ce1edcc2375bf645269e8e4ed0b6d50626c45340
bpiper91 <bpiper91@gmail.com>
2022-10-11 17:30:46 -0400

    added PhotoList component

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -29,7 +29,7 @@ function App() {
     <div>
       <Nav categories={categories} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory}></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
